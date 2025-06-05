5dd21739169d87dfeebf804a78b7641bfeff171e
ZeNeto10 <jrsneto.2005@gmail.com>
2025-06-05 20:02:08 -0300

    Tabelas atualizadas

--- BANCO/01_TABELAS/tabelas.sql
+++ BANCO/01_TABELAS/tabelas.sql
@@ -8,17 +8,7 @@ CREATE TABLE ARYA_USUARIO (
     CONSTRAINT pk_arya_usuario PRIMARY KEY (id_usuario)
 );
 
--- 2. ARYA_ESPECIFICACAO
-CREATE TABLE ARYA_ESPECIFICACAO (
-    id_especificacao VARCHAR2(50) NOT NULL,
-    fabricante VARCHAR2(100),
-    autonomia_minutos NUMBER, 
-    tipo_drone VARCHAR2(100), 
-    modelo VARCHAR2(100),
-    CONSTRAINT pk_arya_especificacao PRIMARY KEY (id_especificacao)
-);
-
--- 3. ARYA_ENDERECO
+-- 2. ARYA_ENDERECO
 CREATE TABLE ARYA_ENDERECO (
     id_endereco VARCHAR2(50) NOT NULL,
     bairro VARCHAR2(100),
@@ -30,7 +20,7 @@ CREATE TABLE ARYA_ENDERECO (
     CONSTRAINT pk_arya_endereco PRIMARY KEY (id_endereco)
 );
 
--- 4. ARYA_AREA_OPERACAO
+-- 3. ARYA_AREA_OPERACAO
 CREATE TABLE ARYA_AREA_OPERACAO (
     id_area_operacao VARCHAR2(50) NOT NULL,
     latitude_central NUMBER NOT NULL,
@@ -38,7 +28,7 @@ CREATE TABLE ARYA_AREA_OPERACAO (
     CONSTRAINT pk_arya_area_operacao PRIMARY KEY (id_area_operacao)
 );
 
--- 5. ARYA_HUB_OPERACIONAL
+-- 4. ARYA_HUB_OPERACIONAL
 CREATE TABLE ARYA_HUB_OPERACIONAL (
     id_hub VARCHAR2(50) NOT NULL,
     nome VARCHAR2(150) NOT NULL,
@@ -47,18 +37,20 @@ CREATE TABLE ARYA_HUB_OPERACIONAL (
     CONSTRAINT pk_arya_hub_operacional PRIMARY KEY (id_hub)
 );
 
--- 6. ARYA_DRONE
+-- 5. ARYA_DRONE
 CREATE TABLE ARYA_DRONE (
     id_drone VARCHAR2(50) NOT NULL,
     id_hub VARCHAR2(50),
     nome VARCHAR2(100) NOT NULL,
     status VARCHAR2(50),
-    id_especificacao VARCHAR2(50) NOT NULL,
+    modelo VARCHAR2(100) NOT NULL,
+    alcanceKM NUMBER,
+    cargaKg NUMBER,
     carregamento VARCHAR2(255),
     CONSTRAINT pk_arya_drone PRIMARY KEY (id_drone)
 );
 
--- 7. ARYA_OCORRENCIA
+-- 6. ARYA_OCORRENCIA
 CREATE TABLE ARYA_OCORRENCIA (
     id_ocorrencia VARCHAR2(50) NOT NULL,
     tipo_ocorrencia VARCHAR2(100) NOT NULL,
@@ -71,7 +63,7 @@ CREATE TABLE ARYA_OCORRENCIA (
     CONSTRAINT pk_arya_ocorrencia PRIMARY KEY (id_ocorrencia)
 );
 
--- 8. ARYA_OUTPUT_MODELO
+-- 7. ARYA_OUTPUT_MODELO
 CREATE TABLE ARYA_OUTPUT_MODELO (
     id_output VARCHAR2(50) NOT NULL,
     nome_modelo_ml VARCHAR2(150),
@@ -82,18 +74,26 @@ CREATE TABLE ARYA_OUTPUT_MODELO (
     CONSTRAINT pk_arya_output_modelo PRIMARY KEY (id_output)
 );
 
--- Relacionamentos com ALTER TABLE
+-- 8. ARYA_MISSAO_DRONE (nova tabela)
+CREATE TABLE ARYA_MISSAO_DRONE (
+    id_missao VARCHAR2(50) NOT NULL,
+    id_drone VARCHAR2(50) NOT NULL,
+    id_ocorrencia VARCHAR2(50) NOT NULL,
+    dataInicio TIMESTAMP NOT NULL,
+    dataFim TIMESTAMP,
+    status VARCHAR2(50),
+    CONSTRAINT pk_arya_missao_drone PRIMARY KEY (id_missao)
+);
+
+-- RELACIONAMENTOS COM ALTER TABLE
 
 -- ARYA_HUB_OPERACIONAL referencia ARYA_ENDERECO
 ALTER TABLE ARYA_HUB_OPERACIONAL
 ADD CONSTRAINT fk_arya_hub_endereco FOREIGN KEY (id_endereco) REFERENCES ARYA_ENDERECO(id_endereco);
 
--- ARYA_DRONE referencia ARYA_HUB_OPERACIONAL e ARYA_ESPECIFICACAO
+-- ARYA_DRONE referencia ARYA_HUB_OPERACIONAL
 ALTER TABLE ARYA_DRONE
 ADD CONSTRAINT fk_arya_drone_hub FOREIGN KEY (id_hub) REFERENCES ARYA_HUB_OPERACIONAL(id_hub);
-
-ALTER TABLE ARYA_DRONE
-ADD CONSTRAINT fk_arya_drone_especificacao FOREIGN KEY (id_especificacao) REFERENCES ARYA_ESPECIFICACAO(id_especificacao);
 
 -- ARYA_OCORRENCIA referencia ARYA_USUARIO, ARYA_ENDERECO, ARYA_AREA_OPERACAO
 ALTER TABLE ARYA_OCORRENCIA
@@ -104,3 +104,10 @@ ADD CONSTRAINT fk_arya_ocorrencia_endereco FOREIGN KEY (id_endereco) REFERENCES 
 
 ALTER TABLE ARYA_OCORRENCIA
 ADD CONSTRAINT fk_ocorrencia_area_operacao FOREIGN KEY (id_area_operacao) REFERENCES ARYA_AREA_OPERACAO(id_area_operacao);
+
+-- ARYA_MISSAO_DRONE referencia ARYA_DRONE e ARYA_OCORRENCIA
+ALTER TABLE ARYA_MISSAO_DRONE
+ADD CONSTRAINT fk_missao_drone_drone FOREIGN KEY (id_drone) REFERENCES ARYA_DRONE(id_drone);
+
+ALTER TABLE ARYA_MISSAO_DRONE
+ADD CONSTRAINT fk_missao_drone_ocorrencia FOREIGN KEY (id_ocorrencia) REFERENCES ARYA_OCORRENCIA(id_ocorrencia);
